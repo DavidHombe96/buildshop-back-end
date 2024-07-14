172f850f91d75c21e574dd6af86f6c9a5293a9e2
DavidHombe96 <90213056+DavidHombe96@users.noreply.github.com>
2024-07-15 00:39:48 +0100

    refactor server

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+require("./utils/dotenv");
+require("./database/dbConnect")
 
 const app = express();
-
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB Connected")).catch((err) => console.log(err));
+const port = 3000;
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({limit: "10mb", extended: true}));
 
-const port = 3000;
 
 app.listen(process.env.PORT || port, () => console.log(`App is running on port ${process.env.PORT}`) )
