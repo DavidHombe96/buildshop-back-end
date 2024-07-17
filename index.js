8a09c330f9dcf01270a91ab65d11290f54efe7f9
DavidHombe96 <90213056+DavidHombe96@users.noreply.github.com>
2024-07-17 21:24:49 +0100

    add authRouter in server

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 require("./utils/dotenv");
 require("./database/dbConnect")
 
+const authRouter = require("./routes/auth")
+
 const app = express();
 const port = 3000;
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({limit: "10mb", extended: true}));
+
+
+app.use('/api/', authRouter)
 
 
 app.listen(process.env.PORT || port, () => console.log(`App is running on port ${process.env.PORT}`) )
